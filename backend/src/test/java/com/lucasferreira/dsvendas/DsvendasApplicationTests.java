d8e35cb3846bc72b7f7bb196449266e9befbda9e
lcseferreira <lcseferreira@hotmail.com>
2021-09-07 08:09:32 -0300

    Project created

---
+++ backend/src/test/java/com/lucasferreira/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasferreira.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
