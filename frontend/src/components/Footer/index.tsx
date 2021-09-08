4a064f187edb426c0d457e13beffe794b449ca2f
lcseferreira <lcseferreira@hotmail.com>
2021-09-08 07:50:34 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Developed by <a href="https://github.com/lcseferreira" target="_blank" rel="noreferrer">Lucas Ferreira</a></p>
+                <p className="text-light"><small><strong>Spring React Week</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
